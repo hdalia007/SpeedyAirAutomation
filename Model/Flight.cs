42f6c223d6a092aa037524cb06a3df2c0ba8e815
Hardik <106388432+hdalia007@users.noreply.github.com>
2023-04-17 22:40:27 -0400

    Add files via upload

---
+++ Model/Flight.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace SpeedyAirAutomation.Model
+{
+    public class Flight
+    {
+        public int Id { get; set; }
+        public int Day { get; set; }
+        public string Departure { get; set; }
+        public string Arrival { get; set; }
+    }
+}
---
+++ Model/Order.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SpeedyAirAutomation.Model
+{
+
+    public static class Order
+    {
+        public static List<OrderData> TotalOrders(string jsonData)
+        {
+            return Orders(jsonData).ConvertAll(x => new OrderData() { OrderId = x.Key, Destination = x.Value });
+        }
+
+        private static Func<string, List<KeyValuePair<string, string>>> Orders = (jsonData)
+            =>
+        {
+            if (string.IsNullOrEmpty(jsonData))
+                throw new ArgumentNullException(nameof(jsonData));
+
+            List<KeyValuePair<string, string>> _tempData = new();
+
+            JObject _Dictionary = JObject.Parse(jsonData);
+
+            foreach (KeyValuePair<string, JToken?> item in _Dictionary)
+                _tempData.Add(new KeyValuePair<string, string>
+                    (item.Key, item.Value["destination"].ToString()));
+
+            return _tempData;
+        };
+
+        public class OrderData : Flight
+        {
+            public string OrderId { get; set; }
+
+            public string Destination { get; set; }
+        }
+
+    }
+}
